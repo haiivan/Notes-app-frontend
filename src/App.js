5b8ee403a44f3560a37f3f9925e512a9d8f9dfb3
HaIvanVillegas <alberto_ha@live.com>
2020-02-10 23:18:16 -0600

    frontend!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import CreateNote from "./components/CreateNote";
@@ -10,11 +10,12 @@ function App() {
   return (
     <Router>
       <Navigation />
-
-      <Route exact path="/" component={NotesList} />
-      <Route path="/edit/:id" component={CreateNote} />
-      <Route path="/create" component={CreateNote} />
-      <Route path="/user" component={CreateUser} />
+      <div className="container p-4">
+        <Route exact path="/" component={NotesList} />
+        <Route path="/edit/:id" component={CreateNote} />
+        <Route path="/create" component={CreateNote} />
+        <Route path="/user" component={CreateUser} />
+      </div>
     </Router>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,39 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
 
 class Navigation extends Component {
   render() {
-    return <div>NAGIGATION</div>;
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            NotesApp
+          </Link>
+
+          <div className="flex" id="navbarNavAltMarkup">
+            <ul className="navbar-nav ml-auto">
+              <li>
+                <Link className="nav-link" to="/">
+                  Notes
+                </Link>
+              </li>
+
+              <li>
+                <Link className="nav-link" to="/create">
+                  Create Note
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/user">
+                  Create User
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
   }
 }
 
